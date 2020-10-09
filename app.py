3312aa6b854ed222b6c627db9ee431f5fc2c9e01
nachakorn1 <72595907+nachakorn1@users.noreply.github.com>
2020-10-09 14:18:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,15 +11,15 @@ def index():
     try:
         f = open("student.csv", "r")
         for line in f.readlines():
-#            print(line)
+            print(line)
             a = line.split(",")
             if(a[0]=="21007"):
-                return a[4]
+               return a[4]
         f.close()
     except Exception:
         return "Could not read to file"
     
-    return "นายอาคม สุวรรณประเสริฐ เลขที่ 0 ชั้น ม.4/"
+    return "นายณชกร ภู่ศรี เลขที่ 29 ชั้น ม.4/15"
 
 @app.route("/webhook", methods=['POST'])
 def webhook():
